42f38072aa18b7c9a40e0fe1f0bb103296bf5b5e
Kyle <kyle.marino22@gmail.com>
2018-08-17 18:17:12 -0700

    code cleanup

--- main.py
+++ main.py
@@ -3,28 +3,31 @@ from requests import get
 from xml.dom import minidom
 import string
 
-
-
-
 class User:
+
 	def __init__(self, name, id, variance):
+
 		self.name = name
 		self.id = id
 		self.variance = variance
 		self.preferences = []
 
-	def addPreference(self, preference):
-		self.preferences.append(preference)
+	def addPreference(self, name, rating):
+
+		self.preferences.append(Preference(name, rating));
 
 	def __repr__(self):
+
 		s = "User: " + self.name + "\nid: " + str(self.id) + "\nvariance: " + str(self.variance) + "\npreferences: "
 		for p in self.preferences:
 			s += str(p)
 		return s
 
 class Game:
+
 	def __init__(self, name, average, bayesaverage, averageweight,
 			minplaytime, maxplaytime, minplayers, maxplayers):
+
 		self.name = name
 		self.average = average
 		self.bayesaverage = bayesaverage
@@ -34,10 +37,8 @@ class Game:
 		self.minplayers = minplayers
 		self.maxplayers = maxplayers
 
-	def getGame(self):
-		return self
-
 	def __repr__(self):
+
 		return ("\nName: " + self.name +
 			"\naverage: " + str(self.average) +
 			"\nbayesaverage: " + str(self.bayesaverage) +
@@ -48,28 +49,15 @@ class Game:
 			"\nmaxplayers: " + str(self.maxplayers))
 
 class Preference:
+
 	def __init__(self, name, userRating):
 		self.name = name
 		self.game = getStats(name)
 		self.userRating = userRating
 
-	def getPreference(self):
-		return self
-
 	def __repr__(self):
+
 		return "Game: " + str(self.game) + "\nuserRating: " + str(self.userRating)
-
-def addGame (name):
-	#Scrapes BGG for game info
-
-	game = Game(name, 7.2, 2.1)
-	return game;
-
-def createPreference (name, userRating):
-
-	game = addGame(name)
-	preference = Preference(game, userRating);
-	return preference
 
 def getStats (name):
 
@@ -103,27 +91,18 @@ def getStats (name):
 		int(minplaytime), int(maxplaytime), int(minplayers), int(maxplayers))
 
 def xmlHandler(xml, tag):
+
 	return xml.getElementsByTagName(tag)[0].firstChild.nodeValue
 
 if __name__== "__main__":
 
-	# g1 = Game("Red7", 7.2, 2.1, 15)
-	# g2 = Game("Catan", 8.2, 2.3, 75)
-	# g3 = Game("Seven Wonders", 7.5, 2.32, 60)
-	# g4 = Game("Pocket Ops", 8.2, 2.3, 10)
-	# g5 = Game("Gravwell", 7.1, 1.7, 20)
-	# g5 = Game("Monopoly", 1.0, 0.1, 700)
-
 
 	u = User("Kyle", 1, 0.5)
-
-	p = Preference("Monopoly", 1)
-
-
-	u.addPreference(p.getPreference)
+	u.addPreference("Monopoly", 1.0)
+	u.addPreference("Catan", 5.7)
+	u.addPreference("Pocket+Ops", 7.3)
 
 	print(u)
 
-	# print(getStats("Catan"))
 
 
